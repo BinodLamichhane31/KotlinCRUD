c5ab773b7c608e80ac5039aaae3b100081b291b3
binodlamiochhane31 <lamibinod31@gmail.com>
2024-06-14 20:16:31 +0545

    Add note and update note design

--- app/src/main/java/com/example/kotlincrud/view/DashboardActivity.kt
+++ app/src/main/java/com/example/kotlincrud/view/DashboardActivity.kt
@@ -1,21 +1,27 @@
 package com.example.kotlincrud.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlincrud.R
+import com.example.kotlincrud.databinding.ActivityDashboardBinding
+import com.example.kotlincrud.databinding.ActivityLoginBinding
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var binding:ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addNodes.setOnClickListener(){
+            startActivity(Intent(this@DashboardActivity,AddNoteActivity::class.java))
         }
+
+
     }
 }
